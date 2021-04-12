df766d1392da6d90ae258415ceb1347f3e6c9710
Anaxagor Tesla <irina-deeva2011.deeva@yandex.ru>
2021-04-12 15:50:42 +0300

    add evo with k2

--- bayesian/train_bn.py
+++ bayesian/train_bn.py
@@ -14,6 +14,70 @@ from bayesian.structure_score import MIG
 from external.pyBN.learning.structure.score.hill_climbing import hc as hc_method
 
 
+import datetime
+import os
+import random
+import pandas as pd
+
+import numpy as np
+from sklearn.metrics import roc_auc_score as roc_auc
+from pgmpy.models import BayesianModel
+from fedot.core.chains.chain_convert import chain_as_nx_graph
+from fedot.core.chains.chain import Chain
+from fedot.core.composer.gp_composer.gp_composer import GPComposerBuilder, GPComposerRequirements
+from fedot.core.composer.optimisers.gp_comp.gp_optimiser import GPChainOptimiserParameters, GeneticSchemeTypesEnum
+from fedot.core.composer.visualisation import ChainVisualiser
+from fedot.core.data.data import InputData
+from fedot.core.repository.model_types_repository import ModelTypesRepository
+from fedot.core.repository.quality_metrics_repository import ClassificationMetricsEnum
+from fedot.core.repository.tasks import Task, TaskTypesEnum
+from fedot.core.utils import project_root
+from pgmpy.estimators import K2Score
+
+
+
+
+
+random.seed(1)
+np.random.seed(1)
+
+def K2(chain: Chain, reference_data: pd.DataFrame) -> float:
+    nodes = reference_data.columns.to_list()
+    graph, labels = chain_as_nx_graph(chain)
+    struct = []
+    for pair in graph.edges():
+        struct.append((str(labels[pair[0]]), str(labels[pair[1]])))
+    BN_model = BayesianModel(struct)
+    no_nodes = []
+    for node in nodes:
+        if node not in BN_model.nodes():
+            no_nodes.append(node)
+    
+    score = K2Score(reference_data).score(BN_model) #+ 1000*len(no_nodes)
+    return score
+
+
+def run_BN_evo(data: pd.DataFrame, max_lead_time: datetime.timedelta = datetime.timedelta(minutes=5), is_visualise=False, with_tuning=False) -> Chain: 
+    available_nodes = ['Tectonic regime', 'Period', 'Lithology', 'Structural setting', 'Gross','Netpay','Porosity','Permeability', 'Depth']
+    composer_requirements = GPComposerRequirements(
+            primary=available_nodes,
+            secondary=available_nodes, max_arity=6,
+            max_depth=3, pop_size=20, num_of_generations=50,
+            crossover_prob=0.8, mutation_prob=0.9, max_lead_time=max_lead_time, add_single_model_chains=False)
+    scheme_type = GeneticSchemeTypesEnum.steady_state
+    optimiser_parameters = GPChainOptimiserParameters(genetic_scheme_type=scheme_type)
+    task = Task(TaskTypesEnum.regression)
+    builder = GPComposerBuilder(task).with_requirements(composer_requirements).with_metrics(K2).with_optimiser_parameters(optimiser_parameters)
+    composer = builder.build()
+    chain_evo_composed = composer.compose_chain(data=data)
+    return chain_evo_composed
+    
+
+
+
+
+
+
 def structure_learning(data: pd.DataFrame, search: str, score: str, node_type: dict, init_nodes: list = None,
                        white_list: list = None,
                        init_edges: list = None, remove_init_edges: bool = True, black_list: list = None) -> dict:
@@ -104,6 +168,54 @@ def structure_learning(data: pd.DataFrame, search: str, score: str, node_type: d
                                                          fixed_edges=init_edges)
             structure = [list(x) for x in list(best_model_mi_mixed.edges())]
             skeleton['E'] = structure
+    if search == 'evo':
+        # if score == "MI":
+        #     column_name_dict = dict([(n, i) for i, n in enumerate(datacol)])
+        #     blacklist_new = []
+        #     for pair in blacklist:
+        #         blacklist_new.append((column_name_dict[pair[0]], column_name_dict[pair[1]]))
+        #     if white_list:
+        #         white_list_old = copy(white_list)
+        #         white_list = []
+        #         for pair in white_list_old:
+        #             white_list.append((column_name_dict[pair[0]], column_name_dict[pair[1]]))
+        #     if init_edges:
+        #         init_edges_old = copy(init_edges)
+        #         init_edges = []
+        #         for pair in init_edges_old:
+        #             init_edges.append((column_name_dict[pair[0]], column_name_dict[pair[1]]))
+        #     bn = hc_method(data.values, restriction=white_list, init_edges=init_edges, remove_geo_edges=remove_init_edges, black_list=blacklist_new)
+        #     structure = []
+        #     nodes = sorted(list(bn.nodes()))
+        #     for rv in nodes:
+        #         for pa in bn.F[rv]['parents']:
+        #             structure.append([list(column_name_dict.keys())[list(column_name_dict.values()).index(pa)],
+        #                           list(column_name_dict.keys())[list(column_name_dict.values()).index(rv)]])
+        #     skeleton['E'] = structure
+        if score == "K2":
+            chain = run_BN_evo(data)
+            graph, labels = chain_as_nx_graph(chain)
+            struct = []
+            for pair in graph.edges():
+                struct.append([str(labels[pair[0]]), str(labels[pair[1]])])
+            skeleton['E'] = struct
+
+        # if score == 'MI_mixed':
+        #     hc_mi_mixed = HillClimbSearch(data, scoring_method=MIG(data=data))
+        #     if init_edges == None:
+        #         best_model_mi_mixed = hc_mi_mixed.estimate(black_list=blacklist, white_list=white_list)
+        #     else:
+        #         if remove_init_edges:
+        #             startdag = DAG()
+        #             startdag.add_nodes_from(nodes=datacol)
+        #             startdag.add_edges_from(ebunch=init_edges)
+        #             best_model_mi_mixed = hc_mi_mixed.estimate(black_list=blacklist, white_list=white_list,
+        #                                                  start_dag=startdag)
+        #         else:
+        #             best_model_mi_mixed = hc_mi_mixed.estimate(black_list=blacklist, white_list=white_list,
+        #                                                  fixed_edges=init_edges)
+        #     structure = [list(x) for x in list(best_model_mi_mixed.edges())]
+        #     skeleton['E'] = structure
     return skeleton
 
 
