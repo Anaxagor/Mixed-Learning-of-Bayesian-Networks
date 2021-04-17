3b8426fa206bb98eb0c4872d57d1adaabcf480d5
Rimmary <banuba@mail.ru>
2021-04-17 09:13:31 +0300

    Ignoring warnings

--- experiments/redef_HC.py
+++ experiments/redef_HC.py
@@ -33,6 +33,7 @@ sys.path.insert(0,parentdir)
 #from scipy.optimize import *
 import numpy as np
 import pandas as pd
+import warnings
 #from heapq import *
 from copy import copy, deepcopy
 
@@ -40,7 +41,7 @@ from external.pyBN.classes.bayesnet import BayesNet
 from external.pyBN.learning.parameter.mle import mle_estimator
 from external.pyBN.learning.parameter.bayes import bayes_estimator
 from external.pyBN.learning.structure.score.info_scores import log_likelihood, AIC
-from bayesian.mi_entropy_gauss import mi_gauss
+
 from external.pyBN.utils.graph import would_cause_cycle
 from pomegranate import BayesianNetwork
 import matplotlib.pyplot as plt
@@ -50,8 +51,8 @@ from bayesian.save_bn import save_params, save_structure, read_params, read_stru
 from bayesian.train_bn import parameter_learning
 from bayesian.calculate_accuracy import calculate_acc
 from external.libpgm.hybayesiannetwork import HyBayesianNetwork
-from experiments.redef_info_scores import log_lik_local, BIC_local as mutual_information
-
+from experiments.redef_info_scores import log_lik_local, BIC_local, AIC_local
+from bayesian.mi_entropy_gauss import mi_gauss   as mutual_information
 
 def hc(data, metric='LL', max_iter=100, debug=False, init_nodes=None, restriction=None, init_edges=None, remove_geo_edges=True, black_list=None):
     """
@@ -168,152 +169,154 @@ def hc(data, metric='LL', max_iter=100, debug=False, init_nodes=None, restrictio
     while improvement:
         improvement = False
         max_delta = 0
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
 
-        if debug:
-            print('ITERATION: ' , _iter)
-        
-        ### TEST ARC ADDITIONS ###
-        for u in bn.nodes():
-            for v in bn.nodes():
-                if v not in c_dict[u] and u!=v and not would_cause_cycle(c_dict, u, v) and len(p_dict[v]) != 3:
-                    # FOR MMHC ALGORITHM -> Edge Restrictions
-                    if (init_nodes is None or  not(v in init_nodes)) and (restriction is None or (u,v) in restriction) and (black_list is None or not((u,v) in black_list)):
-                        # SCORE FOR 'V' -> gaining a parent
-                        old_cols = (v,) + tuple(p_dict[v]) # without 'u' as parent
+            if debug:
+                print('ITERATION: ' , _iter)
+            
+            ### TEST ARC ADDITIONS ###
+            for u in bn.nodes():
+                for v in bn.nodes():
+                    if v not in c_dict[u] and u!=v and not would_cause_cycle(c_dict, u, v) and len(p_dict[v]) != 3:
+                        # FOR MMHC ALGORITHM -> Edge Restrictions
+                        if (init_nodes is None or  not(v in init_nodes)) and (restriction is None or (u,v) in restriction) and (black_list is None or not((u,v) in black_list)):
+                            # SCORE FOR 'V' -> gaining a parent
+                            old_cols = (v,) + tuple(p_dict[v]) # without 'u' as parent
+                            mi_old = mutual_information(data[:,old_cols])
+                            new_cols = old_cols + (u,) # with'u' as parent
+                            mi_new = mutual_information(data[:,new_cols])
+                            delta_score = nrow * (mi_old - mi_new)
+                            
+                            if delta_score > max_delta:
+                                if debug:
+                                    print('Improved Arc Addition: ' , (u,v))
+                                    print('Delta Score: ' , delta_score)
+                                max_delta = delta_score
+                                max_operation = 'Addition'
+                                max_arc = (u,v)
+                                
+
+            # ### TEST ARC DELETIONS ###
+            for u in bn.nodes():
+                for v in bn.nodes():
+                    if v in c_dict[u]:
+                        # SCORE FOR 'V' -> losing a parent
+                        old_cols = (v,) + tuple(p_dict[v]) # with 'u' as parent
                         mi_old = mutual_information(data[:,old_cols])
-                        new_cols = old_cols + (u,) # with'u' as parent
+                        new_cols = tuple([i for i in old_cols if i != u]) # without 'u' as parent
                         mi_new = mutual_information(data[:,new_cols])
                         delta_score = nrow * (mi_old - mi_new)
-                        
-                        if delta_score > max_delta:
-                            if debug:
-                                print('Improved Arc Addition: ' , (u,v))
-                                print('Delta Score: ' , delta_score)
-                            max_delta = delta_score
-                            max_operation = 'Addition'
-                            max_arc = (u,v)
-                            
 
-        # ### TEST ARC DELETIONS ###
-        for u in bn.nodes():
-            for v in bn.nodes():
-                if v in c_dict[u]:
-                    # SCORE FOR 'V' -> losing a parent
-                    old_cols = (v,) + tuple(p_dict[v]) # with 'u' as parent
-                    mi_old = mutual_information(data[:,old_cols])
-                    new_cols = tuple([i for i in old_cols if i != u]) # without 'u' as parent
-                    mi_new = mutual_information(data[:,new_cols])
-                    delta_score = nrow * (mi_old - mi_new)
-
-                    if (delta_score > max_delta):
-                        if init_edges == None:
-                            if debug:
-                                print('Improved Arc Deletion: ' , (u,v))
-                                print('Delta Score: ' , delta_score)
-                            max_delta = delta_score
-                            max_operation = 'Deletion'
-                            max_arc = (u,v)
-                        else:
-                            if (u, v) in init_edges:
-                                if remove_geo_edges:
-                                    if debug:
-                                        print('Improved Arc Deletion: ' , (u,v))
-                                        print('Delta Score: ' , delta_score)
-                                    max_delta = delta_score
-                                    max_operation = 'Deletion'
-                                    max_arc = (u,v)
-                            else:
+                        if (delta_score > max_delta):
+                            if init_edges == None:
                                 if debug:
                                     print('Improved Arc Deletion: ' , (u,v))
                                     print('Delta Score: ' , delta_score)
                                 max_delta = delta_score
                                 max_operation = 'Deletion'
                                 max_arc = (u,v)
-
-        # ### TEST ARC REVERSALS ###
-        for u in bn.nodes():
-            for v in bn.nodes():
-                if v in c_dict[u] and not would_cause_cycle(c_dict,v,u, reverse=True) and len(p_dict[u])!=3 and (init_nodes is None or not (u in init_nodes)) and (restriction is None or (v,u) in restriction) and (black_list is None or not((v,u) in black_list)):
-                    # SCORE FOR 'U' -> gaining 'v' as parent
-                    old_cols = (u,) + tuple(p_dict[v]) # without 'v' as parent
-                    mi_old = mutual_information(data[:,old_cols])
-                    new_cols = old_cols + (v,) # with 'v' as parent
-                    mi_new = mutual_information(data[:,new_cols])
-                    delta1 = -1* nrow * (mi_old - mi_new)
-                    # SCORE FOR 'V' -> losing 'u' as parent
-                    old_cols = (v,) + tuple(p_dict[v]) # with 'u' as parent
-                    mi_old = mutual_information(data[:,old_cols])
-                    new_cols = tuple([u for i in old_cols if i != u]) # without 'u' as parent
-                    mi_new = mutual_information(data[:,new_cols])
-                    delta2 = nrow * (mi_old - mi_new)
-                    # COMBINED DELTA-SCORES
-                    delta_score = delta1 + delta2
-
-                    if (delta_score > max_delta):
-                        if init_edges == None:
-                            if debug:
-                                print('Improved Arc Reversal: ' , (u,v))
-                                print('Delta Score: ' , delta_score)
-                            max_delta = delta_score
-                            max_operation = 'Reversal'
-                            max_arc = (u,v)
-                        else:
-                            if (u, v) in init_edges:
-                                if remove_geo_edges:
+                            else:
+                                if (u, v) in init_edges:
+                                    if remove_geo_edges:
+                                        if debug:
+                                            print('Improved Arc Deletion: ' , (u,v))
+                                            print('Delta Score: ' , delta_score)
+                                        max_delta = delta_score
+                                        max_operation = 'Deletion'
+                                        max_arc = (u,v)
+                                else:
                                     if debug:
-                                        print('Improved Arc Reversal: ' , (u,v))
+                                        print('Improved Arc Deletion: ' , (u,v))
                                         print('Delta Score: ' , delta_score)
                                     max_delta = delta_score
-                                    max_operation = 'Reversal'
+                                    max_operation = 'Deletion'
                                     max_arc = (u,v)
-                            else:
+
+            # ### TEST ARC REVERSALS ###
+            for u in bn.nodes():
+                for v in bn.nodes():
+                    if v in c_dict[u] and not would_cause_cycle(c_dict,v,u, reverse=True) and len(p_dict[u])!=3 and (init_nodes is None or not (u in init_nodes)) and (restriction is None or (v,u) in restriction) and (black_list is None or not((v,u) in black_list)):
+                        # SCORE FOR 'U' -> gaining 'v' as parent
+                        old_cols = (u,) + tuple(p_dict[v]) # without 'v' as parent
+                        mi_old = mutual_information(data[:,old_cols])
+                        new_cols = old_cols + (v,) # with 'v' as parent
+                        mi_new = mutual_information(data[:,new_cols])
+                        delta1 = -1* nrow * (mi_old - mi_new)
+                        # SCORE FOR 'V' -> losing 'u' as parent
+                        old_cols = (v,) + tuple(p_dict[v]) # with 'u' as parent
+                        mi_old = mutual_information(data[:,old_cols])
+                        new_cols = tuple([u for i in old_cols if i != u]) # without 'u' as parent
+                        mi_new = mutual_information(data[:,new_cols])
+                        delta2 = nrow * (mi_old - mi_new)
+                        # COMBINED DELTA-SCORES
+                        delta_score = delta1 + delta2
+
+                        if (delta_score > max_delta):
+                            if init_edges == None:
                                 if debug:
                                     print('Improved Arc Reversal: ' , (u,v))
                                     print('Delta Score: ' , delta_score)
                                 max_delta = delta_score
                                 max_operation = 'Reversal'
                                 max_arc = (u,v)
+                            else:
+                                if (u, v) in init_edges:
+                                    if remove_geo_edges:
+                                        if debug:
+                                            print('Improved Arc Reversal: ' , (u,v))
+                                            print('Delta Score: ' , delta_score)
+                                        max_delta = delta_score
+                                        max_operation = 'Reversal'
+                                        max_arc = (u,v)
+                                else:
+                                    if debug:
+                                        print('Improved Arc Reversal: ' , (u,v))
+                                        print('Delta Score: ' , delta_score)
+                                    max_delta = delta_score
+                                    max_operation = 'Reversal'
+                                    max_arc = (u,v)
 
 
 
 
 
 
-                        
+                            
 
-        if max_delta != 0:
-            improvement = True
-            u,v = max_arc
-            if max_operation == 'Addition':
-                if debug:
-                    print('ADDING: ' , max_arc , '\n')
-                c_dict[u].append(v)
-                p_dict[v].append(u)
+            if max_delta != 0:
+                improvement = True
+                u,v = max_arc
+                if max_operation == 'Addition':
+                    if debug:
+                        print('ADDING: ' , max_arc , '\n')
+                    c_dict[u].append(v)
+                    p_dict[v].append(u)
+                    
+                elif max_operation == 'Deletion':
+                    if debug:
+                        print('DELETING: ' , max_arc , '\n')
+                    c_dict[u].remove(v)
+                    p_dict[v].remove(u)
+                    
+                elif max_operation == 'Reversal':
+                    if debug:
+                        print('REVERSING: ' , max_arc, '\n')
+                    c_dict[u].remove(v)
+                    p_dict[v].remove(u)
+                    c_dict[v].append(u)
+                    p_dict[u].append(v)
                 
-            elif max_operation == 'Deletion':
+            else:
                 if debug:
-                    print('DELETING: ' , max_arc , '\n')
-                c_dict[u].remove(v)
-                p_dict[v].remove(u)
-                
-            elif max_operation == 'Reversal':
-                if debug:
-                    print('REVERSING: ' , max_arc, '\n')
-                c_dict[u].remove(v)
-                p_dict[v].remove(u)
-                c_dict[v].append(u)
-                p_dict[u].append(v)
-            
-        else:
-            if debug:
-                print('No Improvement on Iter: ' , _iter)
+                    print('No Improvement on Iter: ' , _iter)
 
-        ### TEST FOR MAX ITERATION ###
-        _iter += 1
-        if _iter > max_iter:
-            if debug:
-                print('Max Iteration Reached')
-            break
+            ### TEST FOR MAX ITERATION ###
+            _iter += 1
+            if _iter > max_iter:
+                if debug:
+                    print('Max Iteration Reached')
+                break
 
     
     bn = BayesNet(c_dict)
@@ -327,20 +330,24 @@ if __name__ == '__main__':
     data.reset_index(inplace=True, drop=True)
     #columns = ['Period', 'Tectonic regime', 'Hydrocarbon type']
     #columns = ['Gross', 'Netpay','Porosity']
-    #columns = ['Gross', 'Netpay', 'Period']
+    #columns = ['Gross', 'Netpay', 'Period', 'Tectonic regime']
+    #columns = ['Gross','Netpay','Porosity','Permeability', 'Depth']
+
     #columns = data.columns
     columns = ['Tectonic regime', 'Period', 'Lithology', 'Structural setting', 'Hydrocarbon type', 'Gross','Netpay','Porosity','Permeability', 'Depth']
     data_test = data[columns]
     
-    healthcare = pd.read_csv('./datasets/sangiovese.csv')
+    #healthcare = pd.read_csv('./datasets/sangiovese.csv')
+    healthcare = pd.read_csv('./datasets/healthcare.csv')
+    #healthcare = pd.read_csv('./datasets/vk_interests_finance.csv')
     del healthcare['Unnamed: 0']
     #healthcare.to_csv('../datasets/healthcare1.csv')
-    """healthcare = healthcare.iloc[0:500]
+    healthcare = healthcare.iloc[0:500]
     columns = healthcare.columns
     print(columns)
     healthcare.dropna(inplace=True)
     healthcare.reset_index(inplace=True, drop=True)
-    data_test = healthcare"""
+    data_test = healthcare
 
     node_type = get_nodes_type(data_test)
     columns_for_discrete = []
@@ -353,9 +360,9 @@ if __name__ == '__main__':
             columns_for_code.append(param) 
     data_coded, code_dict = code_categories(data_test, "label", columns_for_code)
     if columns_for_discrete != []:
-        data_discereted, est = discretization(data_coded, "kmeans", columns_for_discrete)
+        data_discreted, est = discretization(data_coded, "kmeans", columns_for_discrete)
     else:
-        data_discereted = data_coded
+        data_discreted = data_coded
     
     
     datacol = data_coded.columns.to_list()
@@ -373,39 +380,11 @@ if __name__ == '__main__':
         blacklist_new.append((column_name_dict[pair[0]], column_name_dict[pair[1]]))
     
 
-    """column_name_dict = dict([(n, i) for i, n in enumerate(columns_for_code)])
-    bn = hc(data_coded[columns_for_code])
-    structure = []
-    nodes = sorted(list(bn.nodes()))
-    for rv in nodes:
-        for pa in bn.F[rv]['parents']:
-            structure.append([list(column_name_dict.keys())[list(column_name_dict.values()).index(pa)],
-                            list(column_name_dict.keys())[list(column_name_dict.values()).index(rv)]])
-    skeleton_disc = dict()
-    skeleton_disc['V'] = columns_for_code
-    
-    skeleton_disc['E'] = structure
-    print(skeleton_disc)
-
-    column_name_dict = dict([(n, i) for i, n in enumerate(columns_for_discrete)])
-    bn = hc(data_discereted[columns_for_discrete])
-    structure = []
-    nodes = sorted(list(bn.nodes()))
-    for rv in nodes:
-        for pa in bn.F[rv]['parents']:
-            structure.append([list(column_name_dict.keys())[list(column_name_dict.values()).index(pa)],
-                            list(column_name_dict.keys())[list(column_name_dict.values()).index(rv)]])
-    skeleton_cont = dict()
-    skeleton_cont['V'] = columns_for_discrete
-    
-    skeleton_cont['E'] = structure
-    print(skeleton_cont)"""
-
 
 
     column_name_dict = dict([(n, i) for i, n in enumerate(list(columns))])
     
-    bn = hc(data_discereted, black_list=blacklist_new)
+    bn = hc(data_discreted, black_list=blacklist_new)
     structure = []
     nodes = sorted(list(bn.nodes()))
     for rv in nodes:
